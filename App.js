3f9a3aac70ea856a5b28ee9b9fb2b5d32da1cc66
Ryan Murphy <rcmurphy88@gmail.com>
2025-03-18 00:38:00 -0700

    Initial commit: Set up ScaniApp project

---
+++ App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Scani App!
+        </p>
+      </header>
+    </div>
+  );
+} 
